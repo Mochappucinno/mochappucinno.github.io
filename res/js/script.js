6c7330eca116eec7463ee597598cac9fff5d2235
Faizal Chan <moefc32@gmail.com>
2021-01-17 02:26:12 +0700

    update

--- res/js/script.js
+++ res/js/script.js
@@ -1,10 +1,6 @@
-window.onscroll = function () {
-  scrollIndicator()
-};
-
-function scrollIndicator() {
+window.onscroll = () => {
   let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   let scrolled = (winScroll / height) * 100;
   document.getElementById("bar").style.width = scrolled + "%";
-}
+};
